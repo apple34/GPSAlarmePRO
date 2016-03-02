873c19a44a1827f2347a2bc4f4de4a5e36d88f9f
abnerESC <abner.escocio@gmail.com>
2016-03-02 00:28:37 -0300

    Com erro

--- app/src/main/java/br/com/abner/naopassedalinha3/ServiceBackground.java
+++ app/src/main/java/br/com/abner/naopassedalinha3/ServiceBackground.java
@@ -1,6 +1,8 @@
 package br.com.abner.naopassedalinha3;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
@@ -17,117 +19,84 @@ import com.google.android.gms.location.LocationServices;
 /**
  * Created by Abner on 26/02/2016.
  */
-public class ServiceBackground extends Service implements LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
-    private static final String TAG = "ServiceBackground";
-    private static final int MAX = 10;
-    private boolean running;
-    private int count;
-    public GoogleApiClient mGoogleApiClient;
-    public Location mLastLocation;
-    private Bundle extras;
+public class ServiceBackground extends Service implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private static final String TAG = "BOOMBOOMTESTGPS";
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
 
-    public ServiceBackground () {}
 
-    @Nullable
+    private class MyLocation implements android.location.LocationListener
+    {
+        Location mLastLocation = null;
+        int i = 0;
+
+        public MyLocation(Location location)
+        {
+            Log.e(TAG, "LocationListener " + location);
+            mLastLocation.set(location);
+        }
+
+        public MyLocation () {}
+
+        @Override
+        public void onLocationChanged(Location location)
+        {
+            Log.e(TAG, "onLocationChanged: " + location);
+            mLastLocation.set(location);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider)
+        {
+            Log.e(TAG, "onProviderDisabled: " + provider);
+        }
+
+        @Override
+        public void onProviderEnabled(String provider)
+        {
+            Log.e(TAG, "onProviderEnabled: " + provider);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras)
+        {
+            Log.e(TAG, "onStatusChanged: " + provider);
+        }
+    }
+
     @Override
-    public IBinder onBind(Intent intent) {
+    public IBinder onBind(Intent arg0)
+    {
         return null;
     }
 
     @Override
-    public void onCreate() {
-        Log.i(TAG, " criado");
-        super.onCreate();
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        Log.e(TAG, "onStartCommand");
+        super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
+    }
+
+    @Override
+    public void onCreate()
+    {
         if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(this)
                     .addConnectionCallbacks(this)
                     .addOnConnectionFailedListener(this)
                     .addApi(LocationServices.API)
                     .build();
-
         }
+
         mGoogleApiClient.connect();
     }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.i(TAG, " iniciado");
-        extras = intent.getExtras();
-        Log.i(TAG, "ALL EXTRAS: "+extras.get("location"));
-        count = 0;
-        running = true;
-        return super.onStartCommand(intent, flags, startId);
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mGoogleApiClient.disconnect();
-    }
-
-    class WorkThread extends Thread {
-
-        Location location;
-        public WorkThread (Location location) {
-            this.location = location;
-        }
-
-        @Override
-        public void run() {
-            Log.i(TAG, "WorkThread: "+location);
-
-            super.run();
-
-            try {
-                while (running && count < MAX) {
-                    Log.i(TAG, "Location: "+location.toString());
-                }
-            }catch (Exception e) {
-                Log.i(TAG, e.getMessage(), e);
-            } finally {
-                stopSelf();
-                Log.i(TAG, " encerrado.");
-            }
-        }
-    }
-
-    //****************************************************//
-
-    @Override
-    public void onLocationChanged(Location location) {
-        Intent intent = new Intent(this, ServiceBackground.class);
-        intent.putExtra("location", location);
-        Log.i(TAG, "onLocationChanged( ): " + location.toString());
-        onStartCommand(intent,0,0);
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
-
-    //****************************************************//
-
     @Override
     public void onConnected(Bundle bundle) {
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
                 mGoogleApiClient);
-        if (mLastLocation != null) {
-            Log.i(TAG, "onConnected: "+String.valueOf(mLastLocation.getLatitude())+", "+
-                    String.valueOf(mLastLocation.getLongitude()));
-        }
-
-        onLocationChanged(mLastLocation);
+        Log.e(TAG, "onConnected" + mLastLocation.toString());
+        new MyLocation().onLocationChanged(mLastLocation);
     }
 
     @Override
@@ -139,5 +108,19 @@ public class ServiceBackground extends Service implements LocationListener, Goog
     public void onConnectionFailed(ConnectionResult connectionResult) {
 
     }
-
+    @Override
+    public void onDestroy()
+    {
+//        Log.e(TAG, "onDestroy");
+//        super.onDestroy();
+//        if (mLocationManager != null) {
+//            for (int i = 0; i < mLocationListeners.length; i++) {
+//                try {
+//                    mLocationManager.removeUpdates(mLocationListeners[i]);
+//                } catch (Exception ex) {
+//                    Log.i(TAG, "fail to remove location listners, ignore", ex);
+//                }
+//            }
+//        }
+    }
 }
